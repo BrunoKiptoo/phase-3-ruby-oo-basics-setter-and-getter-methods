698e473f8810e025bf1aaad967ca4b7d73847d50
Bruno <brunochelimo@gmail.com>
2023-02-13 09:17:14 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,39 @@
 class Dog
+    attr_accessor :name
+  
+    def initialize
+      @name = name
+    end
+    
+  end
+  
 
-end
+  class Dog
+    attr_accessor :name
+  
+    def initialize
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+  end
+  
+
+  class Dog
+    attr_accessor :name, :breed
+  
+    def initialize
+      @name = name
+      @breed = name
+    end
+  
+    def breed=(value)
+      @breed = value
+    end
+  end
+
+ 
+  
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    attr_accessor :name
+  
+    def initialize(name = nil)
+      @name = name
+    end
+  end
 
-end
+
+  class Person
+    attr_accessor :name
+  
+    def initialize(name = nil)
+      @name = name
+    end
+  end
+
+
+  class Person
+    attr_accessor :job
+  
+    def initialize(job = nil)
+      @job = job
+    end
+  end
+  
